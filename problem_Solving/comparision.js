108bfffc2052386bda235f23ab0b4714492cca27
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2025-02-07 18:15:37 +0000

    4

---
+++ problem_Solving/comparision.js
@@ -0,0 +1,7 @@
+// how many vowels in a sentence
+
+// const vowels = [1, 2, 3, 3, 4, 5, 5, 6, 7, 8, 9, 10];
+
+let name = "Test";
+
+console.log(name === "Sakib"? "Hello there,  ${name}":"Who are you?");
