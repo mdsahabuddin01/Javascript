ab66c1b002c31d4f7db3c5866c87eaa680057bcb
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2025-02-07 18:13:11 +0000

    1

---
+++ problem_Solving/votable.js
@@ -0,0 +1,13 @@
+// how many vowels in a sentence
+
+// const vowels = [1, 2, 3, 3, 4, 5, 5, 6, 7, 8, 9, 10];
+
+let age = "Test";
+
+age = Number(age);
+
+if (isNaN(age)){
+    console.log("Input is not a number");
+} else {
+    console.log(age < 18? "Too young":"Old enough");
+}
