96dd2c2a9393fa8ab208413d816ebc40dd07dea6
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2025-03-13 20:11:26 +0000

    1 fc

---
+++ JavaScript_Coding_Problems_01/addition.js
@@ -0,0 +1,10 @@
+class Solution {
+
+    addition(a, b) {
+
+        return a + b;
+    }
+}
+
+const solution = new Solution(); // Create an instance of the Solution class
+console.log(solution.addition(1, 2)); // Call the addition method on the instance
