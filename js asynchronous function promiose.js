944947bf8f6ac2278f1ef2bcc74e1a1fdefad7ab
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2025-03-17 00:49:18 +0000

    js asynchronous promiose 02

---
+++ js asynchronous function promiose.js
@@ -0,0 +1,55 @@
+const paymentSuccess = true;
+const marks = 80;
+
+function enroll(){
+    console.log("Enrolled");
+
+    const promise = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            if (paymentSuccess){
+                resolve();
+            } else {
+                reject()
+            }
+        }, 2000);
+    });
+    return promise;
+}
+
+function progress(){
+    console.log("Progressing..");
+
+    const promise = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            if (marks >= 80){
+                resolve();
+            } else {
+                reject("try again!");
+            }
+        }, 2000);
+    })
+    return promise;
+}
+
+function certificate(){
+    console.log("Generated");
+
+    const promise = new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("Congrats!");
+        },2000);
+    });
+    return promise;
+}
+
+
+enroll()
+    .then(progress)
+    .then(certificate)
+    .then(function(value){
+        console.log(value);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+
