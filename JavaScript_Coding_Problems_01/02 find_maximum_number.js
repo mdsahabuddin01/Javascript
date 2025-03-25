a43f6227d12c23b1281550eff1d87aad9ccf4bfb
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2025-03-25 22:25:23 +0000

    Sc

---
+++ JavaScript_Coding_Problems_01/02 find_maximum_number.js
@@ -0,0 +1,24 @@
+//Write a JavaScript program to find the maximum number in an array. 
+
+function findMaxNumber(arr){
+  if (arr.length === 0){
+    console.log("Array is empty");
+  }
+  
+  let max = arr[0];
+  
+  for (let i = 1; i <= arr.length; i++){
+    if (max < arr[i]){
+      max = arr[i];
+    }  
+  }
+  
+  return max;
+}
+
+
+const numbers = [5, 12, 8, 21, 3, 42];
+
+const maxNumber = findMaxNumber(numbers);
+
+console.log("The maximum number is: "+ maxNumber);
