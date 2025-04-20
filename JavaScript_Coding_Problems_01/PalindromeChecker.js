71c07f6b3761d8399ef4f7982800ec850dd9811b
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2025-04-20 21:56:09 +0100

    Palindrome

---
+++ JavaScript_Coding_Problems_01/PalindromeChecker.js
@@ -0,0 +1,24 @@
+function isPalindrome(text){
+  
+  const cleanTxt = text.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+  return cleanTxt === cleanTxt.split().reverse().join("");
+}
+
+const examples = [
+  "Level",
+  "Race car",
+  "No lemon, no melon",
+  "Was it a car or a cat I saw?",
+  "Hello World!",
+  "12321",
+  "Step on no pets"
+];
+
+examples.forEach(str => {
+ if (isPalindrome(str)){
+   console.log(`"${str}" is a palindrome.`);
+ } else {
+   console.log(`"${str}" is a palindrome.`);
+ }
+});
